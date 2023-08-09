b56a6493d9d7f5dc65fb403c4b68807c602556c6
[Akash Shukla] <[akashshukla7458@gmail.com]>
2023-08-09 16:08:10 +0530

    09aug

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
+const PORT =process.env.PORT || 8080
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
@@ -24,7 +25,7 @@ app.get('/', async (req, res) => {
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => console.log('Server started on port 8080'));
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (error) {
     console.log(error);
   }
